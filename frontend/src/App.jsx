985246cbb8bd68972a470fb435bb5ca9c2f65300
Alirio Mieres <mie21001@byui.edu>
2023-10-10 10:59:53 -0400

    delete css code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { AppRouter } from "./router/AppRouter";
-
+import { AuthProvider } from './auth';
+import SVG from "./assets/devchallenges.svg";
+import { UsuariosPage } from "./pages/UsuariosPage";
 function App() {
   return (
     <>
-      {/* <AuthProvider> */}
-      <AppRouter />
-      {/* </AuthProvider> */}
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </>
   );
 }
