8b69f3e5b0ba52c96c2203f032bb0e086ecd91f5
Alirio Mieres <mie21001@byui.edu>
2023-10-10 11:01:39 -0400

    components

--- frontend/src/auth/pages/RegisterPage.jsx
+++ frontend/src/auth/pages/RegisterPage.jsx
@@ -1,36 +1,58 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+// import { AuthContext } from "../context/AuthContext";
 import SVG from "../../assets/devchallenges.svg";
 import facebook from "../../assets/Facebook.svg";
 import google from "../../assets/Google.svg";
 import twitter from "../../assets/Twitter.svg";
 import github from "../../assets/Github.svg";
-export const LoginPage = () => {
-  const { login } = useContext(AuthContext);
+import { useRef } from "react";
+import { createUser } from "../../api/usuarios-service";
+
+export const RegisterPage = () => {
+  const email = useRef("");
+  const password = useRef("");
   const navigate = useNavigate();
 
-  const onLogin = () => {
-    login("Alirio Mieres");
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-    navigate("/", {
+    const user = {
+      usuario: email.current.value,
+      clave: password.current.value,
+      // habilitado: true,
+      // id_persona: 1,
+      // id_rol: 1,
+    };
+    console.log(user);
+
+    await createUser(user);
+
+    navigate("/usuarios", {
       replace: true,
     });
   };
 
   return (
-    <div className="h-screen  flex items-center justify-center">
-      <div className="px-10 border-2 rounded-md text-xl w-[600px] h-[500px] flex flex-col justify-center gap-3">
+    <div className="h-screen flex items-center justify-center m-auto">
+      <div className="px-10 border-2 rounded-md text-xl w-[600px] h-[650px] flex flex-col justify-center gap-3">
         <div>
           <img src={SVG} alt="devchallenges" />
         </div>
-        <h1 className="font-bold mb-3 text-4xl">Login</h1>
-        <form>
+        <h1 className="font-bold mb-3 text-4xl">
+          Join thousands of learners from around the world
+        </h1>
+        <p>
+          Master web development by making real-life projects. There are
+          multiple paths for you to choose
+        </p>
+        <form onSubmit={onSubmit}>
           <div>
             <input
               className="p-2 border-2 rounded-md w-full mb-5"
               type="text"
               placeholder="Email"
+              ref={email}
             />
           </div>
 
@@ -39,13 +61,11 @@ export const LoginPage = () => {
               className="p-2 border-2 rounded-md w-full mb-5"
               type="password"
               placeholder="Password"
+              ref={password}
             />
           </div>
-          <button
-            className="bg-blue-500 text-white w-full p-2 rounded mb-2"
-            onClick={onLogin}
-          >
-            Login
+          <button className="bg-blue-500 text-white w-full p-2 rounded mb-2">
+            Start coding now
           </button>
         </form>
         <span className="align-middle text-center w-full text-gray-600">
@@ -65,6 +85,12 @@ export const LoginPage = () => {
             <img src={github} alt="github_logo" />
           </div>
         </div>
+        <span className="text-center">
+          Adready a member?{" "}
+          <a href="" className="text-blue-400">
+            Login
+          </a>
+        </span>
       </div>
     </div>
   );
--- frontend/src/pages/CreateUsuariosPage.jsx
+++ frontend/src/pages/CreateUsuariosPage.jsx
@@ -2,8 +2,9 @@ import { findAllUsers } from "../api/usuarios-service";
 import { useEffect, useState } from "react";
 import { Navbar } from "../ui/Navbar";
 import { useNavigate } from "react-router-dom";
+import { RegisterPage } from "../auth/pages/RegisterPage";
 
-export const UsuariosPage = () => {
+export const CreateUsuariosPage = () => {
   const [usuarios, setUsuarios] = useState([]);
   const navigate = useNavigate();
 
@@ -22,48 +23,16 @@ export const UsuariosPage = () => {
         console.error("Error al obtener usuarios:", error);
       }
     }
+
     fetchData();
   }, []);
 
   return (
     <div className="flex h-screen">
       <Navbar />
-      <main className="overflow-y-auto p-4">
-        <div className="max-h-screen">
-          <button
-            onClick={onShowForm}
-            className="border-2 rounded bg-blue-500 text-white px-4 py-2"
-          >
-            Añadir
-          </button>
-          <div className="max-h-screen overflow-y-auto">
-            <table className="min-w-full table-auto">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">ID</th>
-                  <th className="px-4 py-2">Usuario</th>
-                  <th className="px-4 py-2">Clave</th>
-                  <th className="px-4 py-2">Habilitado</th>
-                  <th className="px-4 py-2">ID Persona</th>
-                  <th className="px-4 py-2">ID Rol</th>
-                </tr>
-              </thead>
-              <tbody>
-                {usuarios.map((usuario) => (
-                  <tr key={usuario.id}>
-                    <td className="border px-4 py-2">{usuario.id}</td>
-                    <td className="border px-4 py-2">{usuario.usuario}</td>
-                    <td className="border px-4 py-2">{usuario.clave}</td>
-                    <td className="border px-4 py-2">
-                      {usuario.habilitado ? "Sí" : "No"}
-                    </td>
-                    <td className="border px-4 py-2">{usuario.id_persona}</td>
-                    <td className="border px-4 py-2">{usuario.id_rol}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+      <main className="w-full p-4">
+        <div className="">
+          <RegisterPage />
         </div>
       </main>
     </div>
